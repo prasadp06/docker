48b6d248fdac3b1a7e850069150d70034f2c8372
prasadp06 <praspra@starwoodhotels.com>
2016-10-26 15:34:32 -0400

    initial

---
+++ src/entry_point.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Shutdown function
+shut() {
+	kill -s SIGTERM $PID
+	wait $PID
+}
+
+# Status check Function
+check_status() {
+    if [ $# -ne 2 ];
+    then
+        echo "$0: Programming error: Report to prasad.prabhakaran@starwoodhotels.com"
+        exit -1
+    fi
+    exit_code=$1
+    err_msg=$2
+    if [ $exit_code -ne 0 ];
+    then
+        echo $err_msg
+        exit $exit_code
+    fi
+}
+
+#if [ $# -ne 1 ];
+#then
+#   echo "Usage: $0 target_host"
+#    exit 1
+#fi
+
+TargetHost=$1
+TargetProject=$2
+curdir=`pwd`
+ProjectFile=/opt/bin/$TargetProject
+SOAPUI_HOME=/opt/SoapUI/bin
+TEST_RUNNER=$SOAPUI_HOME/testrunner.sh
+
+ls -lrt $SOAPUI_HOME/
+
+if [ ! -f "$ProjectFile" ];
+then
+    echo "Project File does not exist"
+    exit 1
+fi
+
+bash -x ./$TEST_RUNNER -s"SherlockTestSuite" "$ProjectFile"
+PID=$!
+check_status PID "Failed to pass regression testing "
+
+
+trap shut SIGTERM SIGINT
+wait $PID
